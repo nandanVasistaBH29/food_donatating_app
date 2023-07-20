7906d83af2c3149fb38d7e25e822b559312d001f
nandan vasista <nandan@nandan.local>
2023-07-20 08:50:51 +0530

    app ready

---
+++ Downloads/FOOD SHARE/app/src/main/java/bd/edu/rifat/foodDonationApp/MainActivity.java
@@ -0,0 +1,21 @@
+package bd.edu.rifat.foodDonationApp;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+
+//        getSupportActionBar().hide();
+        setContentView(R.layout.activity_main);
+    }
+}
